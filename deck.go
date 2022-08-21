f9662b5289e4bbf8d278462c99db6b9a4963fd20
Federico Baldini <federico.baldini7@gmail.com>
2022-08-21 12:46:01 +0200

    Update deck.go
    Added a new function to create a new deck of cards.

--- deck.go
+++ deck.go
@@ -6,8 +6,23 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Height", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
-	for index, card := range d {
-		fmt.Println(index, card)
+	for i, card := range d {
+		fmt.Println(i+1, card)
 	}
 }
