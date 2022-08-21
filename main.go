9c040b7f9f58def8ab942d530acb1c0a79a65fbe
Federico Baldini <federico.baldini7@gmail.com>
2022-08-21 12:46:11 +0200

    Update main.go
    Used the new function to create a new deck of cards.

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
