ba0d94fb4cbf67937d4235096337ab2cfb961a1d
Federico Baldini <federico.baldini7@gmail.com>
2022-08-29 23:38:09 +0200

    Update deck.go
    Added reading from the file system function.

--- deck.go
+++ deck.go
@@ -42,3 +42,7 @@ func (d deck) toString() string {
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
+
+func newDeckFromFile(filename string) deck {
+
+}
