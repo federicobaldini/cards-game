4dd70581dab8d3466deca99e05378695b96485e0
Federico Baldini <federico.baldini7@gmail.com>
2022-10-14 18:22:27 +0200

    Update deck_test.go

--- deck_test.go
+++ deck_test.go
@@ -12,4 +12,8 @@ func TestNewDeck(t *testing.T) {
 	if d[0] != "Ace of Spades" {
 		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
 	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d)-1])
+	}
 }
