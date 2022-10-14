de81748497e76142893e943121317d6555d2472c
Federico Baldini <federico.baldini7@gmail.com>
2022-10-14 18:18:51 +0200

    Update deck_test.go

--- deck_test.go
+++ deck_test.go
@@ -8,4 +8,8 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
 }
