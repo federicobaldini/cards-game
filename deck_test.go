17e5da1366c8b0e240df52f7ca71e3e8d68d5dd3
Federico Baldini <federico.baldini7@gmail.com>
2022-10-15 11:02:07 +0200

    Update deck_test.go
    Test io deck functions.

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Clubs" {
 		t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deckToSave := newDeck()
+	deckToSave.saveToFile("_decktesting")
+
+	deckToLoad := newDeckFromFile("_decktesting")
+
+	if len(deckToLoad) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(deckToLoad))
+	}
+
+	os.Remove("_decktesting")
 }
